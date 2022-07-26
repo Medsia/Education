d284edc2127f4afa363d018fcafb14d099cc6110
Medsia <108734067+Medsia@users.noreply.github.com>
2022-07-26 22:39:57 +0300

    Add files via upload

---
+++ EpamTraining/lab3/lab3_1/lab3_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Lab3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double R = 3, f;
+            try
+            {
+                Console.WriteLine("Введите -7<=x<=11");
+           double x = Convert.ToDouble(Console.ReadLine());
+                if (x >= -7 && x <= -3)
+                    f = 3;
+                else if (x > -3 && x <= 3) f = -Math.Sqrt(Math.Pow(R, 2) -Math.Pow(x , 2)) + 3;
+                else if (x > 3 && x <= 6) f = -2 * x + 9;
+                else f = x - 9 ;
+                Console.WriteLine("Для x=" + x);
+                Console.WriteLine("f={0}", f);
+            }
+            catch (FormatException)
+            { Console.WriteLine("Неверный ввод"); }
+        }
+    }
+}
---
+++ EpamTraining/lab3/lab3_2/lab3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите точность");
+            double ep = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите начало отрезка");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите конец отрезка");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double x = a, s, y;
+            const int MaxIter = 500;
+            bool done = true;
+            int i;
+            Console.WriteLine(" _______________________");
+            Console.WriteLine("|x|Сумма ряда|n|Зн. функции|");
+            Console.WriteLine(" ________________________");
+            while (x <= b)
+            {
+                s = 1; y = s;
+                done = true;
+                for ( i = 0; Math.Abs((Math.PI / 2 - Math.Atan(s))) > ep; i++)
+                {
+                    s *= (-(x * x) * (2 * i + 1)) / (2 * i + 3);
+                    y += ((Math.PI / 2) + s);
+                    if (i > MaxIter) { done = false; break; }
+                }
+                if (done == true) Console.WriteLine("|{0,5}|{1,18}|{2,5}|{3,18}| ",x,y,i, s);
+
+                else Console.WriteLine("|{0,5}|Ряд расходится| ", x);
+                x = x + h;
+            }
+        }
+    }
+}
