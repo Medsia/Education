88373d18c59247362bc12f9778f03cc2bfd7dd24
Medsia <108734067+Medsia@users.noreply.github.com>
2022-07-26 22:48:43 +0300

    Add files

---
+++ EpamTraining/lab6/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+
+namespace lab6
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант 7\nЗадание 1, 2, 3, 4:");
+            int c = Int32.Parse(Console.ReadLine());
+            int elementsCount;
+            double[] myArray;
+            switch (c)
+            {
+                //Task 1
+                case 1:
+                    double sum = 0;                                      
+                Start1:                   
+                    try
+                    {                     
+                        myArray = InputArray();                     
+                        SummAbs(myArray, ref sum);  // using ref
+                    
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error Message:  {0}", e.Message);
+                        goto Start1;
+                    }
+                    break;
+                // Task 2
+                case 2:
+                    double[] myArray2;
+                    bool isTrue = true;                  
+                    Start2:                  
+                    try
+                    {
+                        myArray = InputArray();
+                        myArray2 = new double[elementsCount];                      
+                        // Sort
+                        for (int i = 0; i < myArray.Length; i++)
+                        {
+                            if (myArray[i] == 0 && isTrue == true )
+                            {
+                                Array.Sort(myArray2);
+                                Array.Reverse(myArray2);
+                                isTrue = false;
+                                
+                            }
+                            else
+                            {                            
+                                myArray2[i]  = myArray[i];         
+                            }
+                        }
+                        
+                        
+
+                        Console.WriteLine("Отсортированные элементы массива по убыванию до первого элемента, равного нулю:  ");
+                        for (int i = 0; i < myArray2.Length; i++)
+                        {
+                            Console.WriteLine(myArray2[i]);
+                            
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Error Message:  {0}", e.Message);
+                        goto Start2;
+                    }
+                        break;
+                // Task 3
+                case 3:
+                Start3:
+                    double[,] myArray3;        
+                    double summ = 0;
+                    Console.WriteLine("Ввод вещественной прямоугольной матрицы n*m: ");
+                    try
+                    {
+                        myArray3 = InputMatrix(out myArray3); //using out
+
+                        Console.WriteLine();
+
+                        OutputMatrix(myArray3);
+                     
+                        //Sum
+                        int flag = 0;
+                        for (int i = 0; i < myArray3.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < myArray3.GetLength(1); j++)
+                                if (myArray3[i,j] < 0)
+                                    flag = 1;
+                            if (flag == 1)
+                            {
+                                for (int z = 0; z < myArray3.GetLength(1); z++)
+                                {
+                                    summ += myArray3[i, z];
+                                    
+                                }
+
+                                Console.WriteLine($"Сумма элементов в тех строках({i}), которые содержат хотя бы один отрицательный \nэлемент: " + summ);
+                            }
+                            flag = 0;
+                            summ = 0;
+                        }
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error Message:  {0}", e.Message);
+                        goto Start3;
+                    }
+                    break;
+
+                // Task 4
+                
+                case 4:
+                    
+                Start4:
+         
+                    try
+                    {
+                        Task4.task4();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error Message:  {0}", e.Message);
+                        goto Start4;
+                    }
+                    break;
+                    
+                default:
+                    Console.WriteLine("Неверное значение");
+                    break;
+            }
+
+            double[] InputArray()
+            {
+                Console.WriteLine("Введите количество элементов массива: ");               
+                
+                elementsCount = int.Parse(Console.ReadLine());
+                myArray = new double[elementsCount];
+
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    Console.WriteLine($"Ввод {i} элемента массива: ");
+                    myArray[i] = Double.Parse(Console.ReadLine());
+                }
+                return myArray;
+            }
+
+            double[,] InputMatrix(out double[,] myArray3) // using out
+            {
+                //double[,] myArray3;
+                int n = 0;
+                int m = 0;
+
+                Console.WriteLine("Ввод n: ");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ввод m: ");
+                m = int.Parse(Console.ReadLine());
+                myArray3 = new double[n, m];
+
+                for (int i = 0; i < myArray3.GetLength(0); i++)
+                {
+                    for (int j = 0; j < myArray3.GetLength(1); j++)
+                    {
+                        Console.WriteLine("Y: " + i + " " + "X: " + j);
+                        myArray3[i, j] = double.Parse(Console.ReadLine());
+                    }
+                }
+                return myArray3;
+            }
+
+            void OutputMatrix(double[,] myArray3)
+            {
+                for (int y = 0; y < myArray3.GetLength(0); y++)
+                {
+                    for (int x = 0; x < myArray3.GetLength(1); x++)
+                    {
+                        Console.Write(myArray3[y, x] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                
+            }
+
+            void SummAbs(double [] myArray,ref double sum) //using ref
+            {
+                for (int i = 0; i < myArray.Length; i++)
+                {
+                    if (myArray[i] < 0)
+                    {
+                        i++;
+                        for (; i < myArray.Length; i++)
+                        {
+                            sum += Math.Abs(myArray[i]);
+
+                        }
+
+                    }
+                }
+                Console.WriteLine($"Сумма модулей элементов массива, расположенных после первого отрицательного элемента:{sum} ");
+            }
+
+            /*double Sum(params double[] parameters) // using Params
+            {
+                double result = 0;
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    result += parameters[i];
+                }
+
+                return result;
+            }*/
+
+
+
+        }
+
+    }
+}
---
+++ EpamTraining/lab6/Task4.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+namespace lab6
+{
+    internal class Task4
+    {
+        struct Filmography
+        {
+            public string title;
+            public string genre;
+            public string country;
+            public double p1, p2, p3;
+        }
+        static Tuple<string, string, double, string> Corteg(string title1, string year1, string country1, double price1)
+        {
+            string p = title1, code, otv;
+            double sum, sr;
+            Filmography[] fg = new Filmography[6];
+            fg[0].title = "Терминатор";
+            fg[0].genre = "Боевик";
+            fg[0].country = "США";
+            fg[0].p1 = 4; fg[0].p2 = 8; fg[0].p3 = 9;
+            fg[1].title = "Брат";
+            fg[1].genre = "Драма,Боевик";
+            fg[1].country = "Россия";
+            fg[1].p1 = 10; fg[1].p2 = 7; fg[1].p3 = 8;
+            fg[2].title = "Бойцовский клуб";
+            fg[2].genre = "Триллер";
+            fg[2].country = "США";
+            fg[2].p1 = 8; fg[2].p2 = 15; fg[2].p3 = 20;
+            fg[3].title = "Большой Стэн";
+            fg[3].genre = "Комедия";
+            fg[3].country = "США";
+            fg[3].p1 = 10; fg[3].p2 = 13; fg[3].p3 = 11;
+            fg[4].title = "Про уродов и людей";
+            fg[4].genre = "Драма";
+            fg[4].country = "Россия";
+            fg[4].p1 = 15; fg[4].p2 = 5; fg[4].p3 = 21;
+            fg[5].title = "Амели";
+            fg[5].genre = "Романтика";
+            fg[5].country = "Франция";
+            fg[5].p1 = 11; fg[5].p2 = 7; fg[5].p3 = 13;
+            int nom = 0;
+            for (int i = 0; i <= 5; i++)
+                if (fg[i].title == title1)
+                {
+                    nom = i;
+                    break;
+                }
+            if (nom != 0)
+            {
+                code = fg[nom].genre + "-" + year1;
+                sum = fg[nom].p1 + fg[nom].p2 + fg[nom].p3 + price1;
+                sr = sum / 4;
+            }
+            else { code = "unknown"; sr = price1; }
+            if (country1 == fg[nom].country) otv = "Да";
+            else otv = "Нет";           
+            return Tuple.Create<string, string, double, string>(p, code, sr, otv);
+        }
+
+            public static void task4()
+        {        
+            Console.WriteLine("Год =");
+            string year = Console.ReadLine();
+            Console.WriteLine("Название =");
+            string title = Console.ReadLine();
+            Console.WriteLine("Страна =");
+            string county = Console.ReadLine();
+            Console.WriteLine("Цена =");
+            double price = Convert.ToDouble(Console.ReadLine());
+            var myTuple = Corteg(title, year, county, price);
+            Console.WriteLine("Название:{0}\n" + "Код: {1}\n"+ "Cредняя цена:{2}\n" + "Совпадает ли страна: {3}\n", myTuple.Item1, myTuple.Item2, myTuple.Item3, myTuple.Item4);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
